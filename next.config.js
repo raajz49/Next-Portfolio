940e7d01f55739a28d20db37fbc81d17a2f39400
Raajz <raj.koirala@cosys.com.np>
2024-10-07 15:44:54 +0545

    change in node module

--- next.config.js
+++ next.config.js
@@ -1,7 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    output: 'export', // This line enables static export
-  }
-  
-  module.exports = nextConfig;
-  
+const nextConfig = {}
+
+module.exports = nextConfig
