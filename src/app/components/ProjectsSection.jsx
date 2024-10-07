b73db06fc47784afff8376a58c6f07fa72a6f522
raajz49 <koiralaraajz@gmail.com>
2024-10-07 07:09:10 +0545

    frontend completed

---
+++ src/app/components/ProjectsSection.jsx
@@ -0,0 +1,137 @@
+"use client";
+import React, { useState, useRef } from "react";
+import ProjectCard from "./ProjectCard";
+import ProjectTag from "./ProjectTag";
+import { motion, useInView } from "framer-motion";
+
+const projectsData = [
+  {
+    id: 1,
+    title: "Stabilisation",
+    description: "This is a web app for with user blogging section and various other features of UNDP Stabilisation",
+    image: "/images/projects/image.png",
+    tag: ["All", "Web"],
+    gitUrl: "https://stabilisation.org/",
+    previewUrl: "https://stabilisation.org/",
+  },
+  {
+    id: 2,
+    title: "Scalper Investment",
+    description: "This is a chat app where various servers with variety of channelgroups and channels are available  ",
+    image: "/images/projects/discord.jpg",
+    tag: ["All", "Web"],
+    gitUrl: "https://scalperinvestment.com/",
+    previewUrl:"https://scalperinvestment.com/",
+  },
+  {
+    id: 3,
+    title: "User-Blog Website",
+    description: "This is kind of a blogging site where user can create regular blogs and view other blogs too",
+    image: "/images/4.png",
+    tag: ["All", "Web"],
+    gitUrl: "/",
+    previewUrl: "/",
+  },
+  {
+    id: 4,
+    title: "E-commerce Admin Website",
+    description: "This is the E-commerce admin dashboard made in Next.JS",
+    image: "/images/projects/3.png",
+    tag: ["All", "Web"],
+    gitUrl: "/",
+    previewUrl: "/",
+  },
+  {
+    id: 5,
+    title: "Trek-Planner Website",
+    description: "This is the UI designed for the implementation for making a trek planning website",
+    image: "/images/3.png",
+    tag: ["All", "web"],
+    gitUrl: "/",
+    previewUrl: "/",
+  },
+  // {
+  //   id: 5,
+  //   title: "React Firebase Template",
+  //   description: "Authentication and CRUD operations",
+  //   image: "/images/projects/5.png",
+  //   tag: ["All", "Web"],
+  //   gitUrl: "/",
+  //   previewUrl: "/",
+  // },
+  // {
+  //   id: 6,
+  //   title: "Full-stack Roadmap",
+  //   description: "Project 5 description",
+  //   image: "/images/projects/6.png",
+  //   tag: ["All", "Web"],
+  //   gitUrl: "/",
+  //   previewUrl: "/",
+  // },
+];
+
+const ProjectsSection = () => {
+  const [tag, setTag] = useState("All");
+  const ref = useRef(null);
+  const isInView = useInView(ref, { once: true });
+
+  const handleTagChange = (newTag) => {
+    setTag(newTag);
+  };
+
+  const filteredProjects = projectsData.filter((project) =>
+    project.tag.includes(tag)
+  );
+
+  const cardVariants = {
+    initial: { y: 50, opacity: 0 },
+    animate: { y: 0, opacity: 1 },
+  };
+
+  return (
+    <section id="projects">
+      <h2 className="text-center text-4xl font-bold text-white mt-4 mb-8 md:mb-12">
+        My Projects
+      </h2>
+      <div className="text-white flex flex-row justify-center items-center gap-2 py-6">
+        <ProjectTag
+          onClick={handleTagChange}
+          name="All"
+          isSelected={tag === "All"}
+        />
+        {/* <ProjectTag
+          onClick={handleTagChange}
+          name="Web"
+          isSelected={tag === "Web"}
+        />
+        <ProjectTag
+          onClick={handleTagChange}
+          name="Mobile"
+          isSelected={tag === "Mobile"}
+        /> */}
+      </div>
+      <ul ref={ref} className="grid md:grid-cols-3 gap-8 md:gap-12">
+        {filteredProjects.map((project, index) => (
+          <motion.li
+            key={index}
+            variants={cardVariants}
+            initial="initial"
+            animate={isInView ? "animate" : "initial"}
+            transition={{ duration: 0.3, delay: index * 0.4 }}
+          >
+            <ProjectCard
+              key={project.id}
+              title={project.title}
+              description={project.description}
+              imgUrl={project.image}
+              gitUrl={project.gitUrl}
+              previewUrl={project.previewUrl}
+            />
+          </motion.li>
+        ))}
+      </ul>
+    </section>
+  );
+};
+
+export default ProjectsSection;
