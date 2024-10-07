cf19c9452d7beff6ff5c0847a2140783ad2a51e8
raajz49 <koiralaraajz@gmail.com>
2024-10-07 23:04:28 +0545

    changes in next.config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  basePath: "/Next-Portfolio", // Set the base path to match the GitHub Pages subdirectory
+  assetPrefix: "/Next-Portfolio/", // Prefix all static assets with this path
+  images: {
+    unoptimized: true, // Disable Next.js image optimization to work better with GitHub Pages
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
