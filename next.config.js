769257f144220081ba4765ab05fa5063d32d80ec
raajz49 <koiralaraajz@gmail.com>
2024-10-07 23:17:42 +0545

    fixed the basepath

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  basePath: "/Next-Portfolio", // Set the base path to match the GitHub Pages subdirectory
-  assetPrefix: "/Next-Portfolio/", // Prefix all static assets with this path
-  images: {
-    unoptimized: true, // Disable Next.js image optimization to work better with GitHub Pages
-  },
+  basePath: "/Next-Portfolio",
 };
 
 module.exports = nextConfig;
