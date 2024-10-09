2b0527989dcee954eae9439714c27c13e84a8f60
raajz49 <koiralaraajz@gmail.com>
2024-10-09 08:27:21 +0545

    changes in module

--- next.config.js
+++ next.config.js
@@ -1,11 +1,4 @@
-// next.config.js
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  basePath: "/Next-Portfolio", // Define the base path for your project
-  assetPrefix: "/Next-Portfolio/", // This prefixes all static assets with the correct path
-  images: {
-    unoptimized: true, // Optional: Disable Next.js's image optimization for static hosting
-  },
-};
+const nextConfig = {};
 
 module.exports = nextConfig;
