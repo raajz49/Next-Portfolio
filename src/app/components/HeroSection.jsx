2fb8d93138c49e6ee11189738f57632e067bfa70
raajz49 <koiralaraajz@gmail.com>
2024-10-16 17:42:20 +0545

    cv changed

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -48,7 +48,7 @@ const HeroSection = () => {
             </Link>
             <Link
                  href={
-                  "https://drive.google.com/file/d/1a7w79O8w19ELg_sSeblpn-UUZt-moo17/view?usp=sharing"
+                  "https://drive.google.com/file/d/1Q0meFc_ypHdSLHyF2FB9EcRLL7IFauBT/view"
                 }
                 target="_blank"
               className="px-1 inline-block py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-800 text-white mt-3"
